09d38b4e701bf560c87724dc80348323b130699c
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2017-02-21 01:18:38 +0000

    completed send and fetch methods and passed test

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,36 @@
 // YOUR CODE HERE:
+var app = {};
+
+app.server = 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages';
+
+app.init = function() {
+
+};
+
+app.send = function(message) {
+  $.ajax({
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(data) {
+      console.log('chatterbox: message sent, ' + data);
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    url: app.server,
+    type: 'GET',
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  });
+};
